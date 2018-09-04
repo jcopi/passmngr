5c75743cb4e39afe858c8a4f7f2967308021a297
Joel Copi <joel_copi_midstatestechnical@whirlpool.com>
2018-09-04 16:07:58 -0400

    Updates to record

--- typescript/records.ts
+++ typescript/records.ts
@@ -1,10 +1,12 @@
+import { StringEncoding } from "./build/core";
+
 export class UserSettings {
 
 }
 
 interface URLP  {
-    allowSubdomains: false;
-    allowLongerPaths: false;
+    allowSubdomains: boolean;
+    allowLongerPaths: boolean;
     domainPattern: string[];
     pathPattern: string[];
 }
@@ -24,6 +26,7 @@ function isURLP (arg: any): arg is URLP {
 interface PMRecord {
     urlFilter: URLP;
     appID: string;
+    tag: string;
     identity: string;
     password: string;
     formData: object;
@@ -34,6 +37,8 @@ function isPMRecord (arg: any): arg is PMRecord {
         return false;
     if (!("appID" in arg) || typeof arg.appID != "string")
         return false;
+    if (!("tag" in arg) || typeof arg.tag != "string")
+        return false;
     if (!("identity" in arg) || typeof arg.identity != "string")
         return false;
     if (!("password" in arg) || typeof arg.password != "string")
@@ -41,4 +46,127 @@ function isPMRecord (arg: any): arg is PMRecord {
     if (!("formData" in arg) || typeof arg.formData != "object")
         return false;
     return true;
+}
+
+export class URLPattern {
+    internal: URLP;
+    constructor (arg?: URLP) {
+        if (typeof arg == "undefined" || !isURLP(arg))
+            arg = {"allowSubdomains":false, "allowLongerPaths": false, "domainPattern":[], "pathPattern":[]};
+        this.internal = arg;            
+    }
+
+    toObject (): URLP {
+        return this.internal;
+    }
+    fromObject (obj: URLP): void {
+        this.internal = obj;
+    }
+
+    setDomain (allowSubs: boolean, pattern: string[]): void {
+        this.internal.allowSubdomains = allowSubs;
+        this.internal.domainPattern = pattern;
+    }
+
+    setPath (allowLonger: boolean, pattern: string[]): void {
+        this.internal.allowLongerPaths = allowLonger;
+        this.internal.pathPattern = pattern;
+    }
+
+    testURL (url: string): boolean {
+        let U;
+        try {
+            U = new URL(url);
+        } catch (ex) {
+            return false;
+        }
+        let domArr = U.hostname.split(".");
+        let pathArr = U.pathname.split("/");
+
+        if (this.internal.allowSubdomains) {
+            if (this.internal.domainPattern.length > domArr.length)
+                return false;
+        } else {
+            if (this.internal.domainPattern.length != domArr.length)
+                return false;
+        }
+        for (let i = 1; i <= this.internal.domainPattern.length; ++i) {
+            let j = this.internal.domainPattern.length - i;
+            let k = domArr.length - i;
+            if (this.internal.domainPattern[j] == "*")
+                continue;
+            if (this.internal.domainPattern[j] != domArr[k])
+                return false;
+        }
+
+        if (this.internal.allowLongerPaths) {
+            if (this.internal.pathPattern.length > pathArr.length)
+                return false;
+        } else {
+            if (this.internal.pathPattern.length != pathArr.length)
+                return false;
+        }
+
+        for (let i = 0; i < this.internal.pathPattern.length; ++i) {
+            if (this.internal.pathPattern[i] == "*")
+                continue;
+            if (this.internal.pathPattern[i] != pathArr[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    suggestedPattern (url: string): URLP {
+        let result;
+        let U;
+        U = new URL(url);
+        result.allowLongerPaths = true;
+        result.pathPattern = [];
+        let domArr = U.hostname.split(".");
+        if (domArr.length > 2) {
+            result.domainPattern = domArr;
+            result.domainPattern[0] = "*";
+            result.allowSubdomains = false;
+        } else {
+            result.domainPattern = domArr;
+            result.allowSubdomains = true;
+        }
+
+        return result;
+    }
+}
+
+export class PasswordRecords {
+    records: PMRecord[];
+
+    constructor () {
+        this.records = [];
+    }
+    toObject (): PMRecord[] {
+        return this.records;
+    }
+    fromObject (obj: PMRecord[]): void {
+        this.records = obj;
+    }
+
+    getRecords (url: string): PMRecord[] {
+        return this.records.filter(r => new URLPattern(r.urlFilter).testURL(url));
+    }
+    addRecord (record: PMRecord): void {
+        this.records.push(record);
+    }
+
+    toFile (): ArrayBuffer {
+        let tmp = JSON.stringify(this.records);
+        return StringEncoding.toUTF8(tmp);
+    }
+
+    fromFile (file: ArrayBuffer): void {
+        let tmp = StringEncoding.fromUTF8(file);
+        let obj = JSON.parse(tmp);
+        if (!Array.isArray(obj))
+            throw "Invalid Password Record File";
+        this.fromObject(obj.filter(r => isPMRecord(r)));
+    }
 }
