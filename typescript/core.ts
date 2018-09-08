efe9bef6589780e612e3f0ec80bd6f7aff61a673
Joel Copi <j.copi@outlook.com>
2018-09-08 11:17:08 -0400

    changes

--- typescript/core.ts
+++ typescript/core.ts
@@ -1,6 +1,5 @@
-
-export class Base64 {
-    static serialize (buffer: ArrayBuffer): string {
+export class BufferEncoding {
+    static toBase64 (buffer: ArrayBuffer): string {
         let result = "";
         let encoding = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
@@ -28,8 +27,7 @@ export class Base64 {
 
         return result;
     }
-
-    static parse (b64: string): ArrayBuffer {
+    static fromBase64 (b64: string): ArrayBuffer {
         let encodings = {"0":52,"1":53,"2":54,"3":55,"4":56,"5":57,"6":58,"7":59,"8":60,"9":61,"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,
 		"G":6,"H":7,"I":8,"J":9,"K":10,"L":11,"M":12,"N":13,"O":14,"P":15,"Q":16,"R":17,"S":18,"T":19,"U":20,"V":21,"W":22,"X":23,"Y":24,"Z":25,"a":26,"b":27,"c":28,"d":29,"e":30,"f":31,"g":32,"h":33,"i":34,"j":35,"k":36,"l":37,"m":38,"n":39,"o":40,
         "p":41,"q":42,"r":43,"s":44,"t":45,"u":46,"v":47,"w":48,"x":49,"y":50,"z":51,"+":62,"/":63};
@@ -59,6 +57,12 @@ export class Base64 {
         }
 
         return bytes.buffer;
+    }
+    static toCustomBase (buffer: ArrayBuffer, dict: string): string {
+        let result = "";
+        let bits = Math.floor(Math.log2(dict.length) + 1);
+        
+        return result;
     }
 }
 
